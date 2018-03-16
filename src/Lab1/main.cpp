7c11ccc8928308aec53b8182a78e620564ab89c3
ableigdev <igor.yarovoy.98@ukr.net>
2018-03-16 22:53:25 +0200

    main for sort list

--- src/Lab1/main.cpp
+++ src/Lab1/main.cpp
@@ -1,104 +1,26 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "List.h"
 #include "Student.h"
 
 int main()
 {
+
 	{
-		Student stud1;
-		Student stud2;
-
-		std::cout << "Enter data: ";
-		std::cin >> stud1;
-		std::cout << "Student: " << stud1;
-
-		stud2 = stud1;
-
-		Student stud3(stud2);
-
-		if (stud1 == stud2)
-			std::cout << "stud1 == stud2" << std::endl;
-
-		if (stud1 == stud3)
-			std::cout << "stud1 == stud3" << std::endl;
-
-		if (stud2 == stud3)
-			std::cout << "stud2 == stud3" << std::endl;
-
-		if (stud2 >= stud3)
-			std::cout << "stud2 >= stud3" << std::endl;
-
-		if (stud2 <= stud3)
-			std::cout << "stud2 <= stud3" << std::endl;
-
-
-		stud2.setSurname("Aaaaaaa");
-		stud2.setName("AAAaaa");
-		stud2.setLastname("Aaaaaa");
-
-		if (stud2 > stud1)
-			std::cout << "stud2 > stud1" << std::endl;
 		
-		if (stud2 >= stud1)
-			std::cout << "stud2 >= stud1" << std::endl;
 
-		if (stud2 != stud1)
-			std::cout << "stud2 != stud1" << std::endl;
+		std::srand(time(0));
 
-		stud3.setSurname("Zzzzzzzzzzzzzzzz");
-		stud3.setName("Zzzzzz");
-		stud3.setLastname("Zzzzzzz");
+		List<int> list5;
 
-		if (stud3 < stud2)
-			std::cout << "stud3 < stud2" << std::endl;
-
-		if (stud3 <= stud2)
-			std::cout << "stud3 <= stud2" << std::endl;
-
-		if (stud3 != stud2)
-			std::cout << "stud3 != stud2" << std::endl;
-
-		std::cin.get();
-	}
-	{
-		List<int> list1;
-
-		list1.pushInSortList(3);
-		list1.pushInSortList(4);
-		list1.pushInSortList(1);
-		list1.pushInSortList(8);
-		list1.pushInSortList(4);
-		list1.pushInSortList(6);
-		list1.pushInSortList(8);
-		
-		list1.print();
-
-		List<int> list2(list1);
-		list2.print();
-
-		List<int> list3;
-		list3 = list2;
-		list3.print();
-
-		for (int i = 0; i < 4; ++i)
-			std::cout << ++list1 << ' ';
-
-		std::cout << std::endl;
-
-		list1.setCurrentNodeOnTheBegin();
-
-		for (int i = 0; i < 4; ++i)
-			std::cout << --list1 << ' ';
+		for (int i = 0; i < 1000000; ++i)
+		{
+			list5.pushBack(rand() % 100);
+		}
 
 	}
-	{
-		List<int> list1;
 
-		for (int i = 0; i < 5; ++i)
-			list1.pushBack(i);
-
-		list1.print();
-	}
 	std::cin.get();
 
 	return 0;
