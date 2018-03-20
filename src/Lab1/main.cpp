b1fdb5cbd92260ccab08b5b9475d50583bfa7f80
ableigdev <igor.yarovoy.98@ukr.net>
2018-03-21 00:35:28 +0200

    Test for NameList

--- src/Lab1/main.cpp
+++ src/Lab1/main.cpp
@@ -4,10 +4,43 @@
 #include "List.h"
 #include "ListIterator.h"
 #include "Student.h"
+#include "NameList.h"
 
 
 int main()
 {
+	NameList<int> list("first");
+
+
+	for (int i = 0; i < 10; ++i)
+	{
+		list.pushFront(i);
+	}
+
+	std::cout << list << std::endl;
+
+	list.sort();
+
+	std::cout << list << std::endl;
+
+	std::cout << "name: " << list.getNameClassList() << std::endl;
+	
+
+	NameList<int> list2;
+
+	list2.pushInSortList(3);
+	list2.pushInSortList(1);
+	list2.pushInSortList(8);
+	list2.pushInSortList(5);
+	list2.pushInSortList(2);
+	list2.pushInSortList(3);
+	list2.pushInSortList(4);
+
+	std::cout << list2 << std::endl;
+
+	list2.setNameClassList("second");
+
+	std::cout << "name: " << list2.getNameClassList() << std::endl;
 
 
 	std::cin.get();
