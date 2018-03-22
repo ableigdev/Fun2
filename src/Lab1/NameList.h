f530d51b22e0808ff0a98af735972068d8a76b13
ableigdev <igor.yarovoy.98@ukr.net>
2018-03-23 01:14:32 +0200

    Refactoring class NameList

--- src/Lab1/NameList.h
+++ src/Lab1/NameList.h
@@ -3,51 +3,45 @@
 #include "List.h"
 #include <string>
 
-template <typename NODETYPE>
+template <typename NODETYPE, typename TYPESTRING>
 class NameList : public List<NODETYPE>
 {
 public:
-	explicit NameList(const std::string& = "");
-	explicit NameList(const NameList<NODETYPE>&);
-	virtual ~NameList();
+	explicit NameList(const std::basic_string<TYPESTRING>& = "");
+	explicit NameList(const NameList<NODETYPE, TYPESTRING>&);
+	virtual ~NameList() = default;
 
-	void setNameClassList(const std::string&);
-	std::string getNameClassList() const;
-
+	void setNameClassList(const std::basic_string<TYPESTRING>&);
+	std::basic_string<TYPESTRING> getNameClassList() const;
 private:
-	std::string m_NameList;
+	std::basic_string<TYPESTRING> m_NameList;
 };
 
 
-template <typename NODETYPE>
-NameList<NODETYPE>::NameList(const std::string& str)
+template <typename NODETYPE, typename TYPESTRING>
+NameList<NODETYPE, TYPESTRING>::NameList(const std::basic_string<TYPESTRING>& str)
 	: List(),
 	m_NameList(str)
 {
 
 }
 
-template <typename NODETYPE>
-NameList<NODETYPE>::NameList(const NameList<NODETYPE>& right)
-	: m_NameList(right.m_NameList)
+template <typename NODETYPE, typename TYPESTRING>
+NameList<NODETYPE, TYPESTRING>::NameList(const NameList<NODETYPE, TYPESTRING>& right)
+	: List(),
+	m_NameList(right.m_NameList)
 {
 	*this = right;
 }
 
-template <typename NODETYPE>
-NameList<NODETYPE>::~NameList()
-{
-	m_NameList.clear();
-}
-
-template <typename NODETYPE>
-void NameList<NODETYPE>::setNameClassList(const std::string& str)
+template <typename NODETYPE, typename TYPESTRING>
+void NameList<NODETYPE, TYPESTRING>::setNameClassList(const std::basic_string<TYPESTRING>& str)
 {
 	m_NameList = str;
 }
 
-template <typename NODETYPE>
-std::string NameList<NODETYPE>::getNameClassList() const
+template <typename NODETYPE, typename TYPESTRING>
+std::basic_string<TYPESTRING> NameList<NODETYPE, TYPESTRING>::getNameClassList() const
 {
 	return m_NameList;
 }
