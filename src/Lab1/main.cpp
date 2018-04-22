193c76c7fcabfb236489f52669c77e3beba11928
ableigdev <igor.yarovoy.98@ukr.net>
2018-04-22 12:43:55 +0300

    Added Unicode macro

--- src/Lab1/main.cpp
+++ src/Lab1/main.cpp
@@ -5,30 +5,65 @@
 #include "ListIterator.h"
 #include "Student.h"
 #include "NameList.h"
+#include "blist.h"
+
+#ifdef UNICODE
+#define out std::wcout
+#else
+#define out std::cout
+#endif // UNICODE
 
 
 int main()
 {
 	std::srand(time(0));
-	List<int> lists;
-
-	for (int i = 0; i < 10; ++i)
+	
 	{
-		lists.pushFront(rand() % 100);
+		TBList<int> list;
+
+		for (int i = 0; i < 1000000; ++i)
+		{
+			list.AddBeforeHead(rand() % 3000000);
+		}
+
+		time_t start = clock();
+		list.SortListBiDir();
+		time_t finish = clock();
+
+		out << "time: " << finish - start << std::endl;
+		out << "Counter compare: " << list.getCounterCompare() << std::endl;
+		out << "Counter recursive call: " << list.getCounterRecursiveCall() << std::endl;
+	}
+	{
+		
+		List<int> lists;
+
+		for (int i = 0; i < 1000000; ++i)
+		{
+			lists.pushFront(rand() % 3000000);
+		}
+
+		time_t start = clock();
+		lists.sort();
+		time_t finish = clock();
+
+		out << "time: " << finish - start << std::endl;
+
+		if (lists.bubbleSort())
+		{
+			out << "Sort good" << std::endl;
+		}
+		else
+		{
+			out << "Sort bad" << std::endl;
+		}
+
+		out << "Counter compare: " << lists.getCounterCompare() << std::endl;
+		out << "Counter recursive call: " << lists.getCounterRecursiveCall() << std::endl;
 	}
 
-
-	lists.sort();
-	lists.print();
-	int* value = &lists.getReferencesCurrentData();
-
-	*value = 30;
-
-	lists.sortCurrentNodePtr();
-
-	lists.print();
-
 	std::cin.get();
+	system("pause");
 
 	return 0;
 }
