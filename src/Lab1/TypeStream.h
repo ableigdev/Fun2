512be28d0bb2eacd8fe1dfff9ceb68f3e45eae9d
ableigdev <igor.yarovoy.98@ukr.net>
2018-04-23 16:17:27 +0300

    Added default type for Unicode and ANSI

--- src/Lab1/TypeStream.h
+++ src/Lab1/TypeStream.h
@@ -6,8 +6,10 @@
 #define listStream std::wostream
 #define listInStream std::wistream
 #define out std::wcout
+#define TYPESTRING wchar_t
 #else
 #define listStream std::ostream
 #define listInStream std::istream
 #define out std::cout
+#define TYPESTRING char
 #endif
