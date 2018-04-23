d316a71ad31c9740c1fbc09bd1cfb4e5f56d593b
ableigdev <igor.yarovoy.98@ukr.net>
2018-04-23 16:24:43 +0300

    Added comparison definitions for operators and deleted template string type

--- src/Lab1/NameList.h
+++ src/Lab1/NameList.h
@@ -3,53 +3,60 @@
 #include "List.h"
 #include <string>
 
-template <typename NODETYPE, typename TYPESTRING>
+template <typename NODETYPE>
 class NameList : public List<NODETYPE>
 {
 public:
 	explicit NameList(const std::basic_string<TYPESTRING>& = "");
-	explicit NameList(const NameList<NODETYPE, TYPESTRING>&);
+	explicit NameList(const NameList<NODETYPE>&);
 	virtual ~NameList() = default;
 
 	void setNameClassList(const std::basic_string<TYPESTRING>&);
 	std::basic_string<TYPESTRING> getNameClassList() const;
 
-	const NameList<NODETYPE, TYPESTRING>& operator=(const NameList<NODETYPE, TYPESTRING>&);
+	const NameList<NODETYPE>& operator=(const NameList<NODETYPE>&);
+
+	bool operator==(const NameList<NODETYPE>&) const;
+	bool operator!=(const NameList<NODETYPE>&) const;
+	bool operator>=(const NameList<NODETYPE>&) const;
+	bool operator<=(const NameList<NODETYPE>&) const;
+	bool operator>(const NameList<NODETYPE>&) const;
+	bool operator<(const NameList<NODETYPE>&) const;
 private:
 	std::basic_string<TYPESTRING> m_NameList;
 };
 
 
-template <typename NODETYPE, typename TYPESTRING>
-NameList<NODETYPE, TYPESTRING>::NameList(const std::basic_string<TYPESTRING>& str)
+template <typename NODETYPE>
+NameList<NODETYPE>::NameList(const std::basic_string<TYPESTRING>& str)
 	: List(),
 	m_NameList(str)
 {
 
 }
 
-template <typename NODETYPE, typename TYPESTRING>
-NameList<NODETYPE, TYPESTRING>::NameList(const NameList<NODETYPE, TYPESTRING>& right)
+template <typename NODETYPE>
+NameList<NODETYPE>::NameList(const NameList<NODETYPE>& right)
 	: List(),
 	m_NameList(right.m_NameList)
 {
 	*this = right;
 }
 
-template <typename NODETYPE, typename TYPESTRING>
-void NameList<NODETYPE, TYPESTRING>::setNameClassList(const std::basic_string<TYPESTRING>& str)
+template <typename NODETYPE>
+void NameList<NODETYPE>::setNameClassList(const std::basic_string<TYPESTRING>& str)
 {
 	m_NameList = str;
 }
 
-template <typename NODETYPE, typename TYPESTRING>
-std::basic_string<TYPESTRING> NameList<NODETYPE, TYPESTRING>::getNameClassList() const
+template <typename NODETYPE>
+std::basic_string<TYPESTRING> NameList<NODETYPE>::getNameClassList() const
 {
 	return m_NameList;
 }
 
-template <typename NODETYPE, typename TYPESTRING>
-const NameList<NODETYPE, TYPESTRING>& NameList<NODETYPE, TYPESTRING>::operator=(const NameList<NODETYPE, TYPESTRING>& right)
+template <typename NODETYPE>
+const NameList<NODETYPE>& NameList<NODETYPE>::operator=(const NameList<NODETYPE>& right)
 {
 	if (this != &right)
 	{
