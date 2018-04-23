0823fac45c097afff185aa2fc2cc11f28a5b1532
ableigdev <igor.yarovoy.98@ukr.net>
2018-04-23 16:21:12 +0300

    Deleted template type string

--- src/Lab1/Student.h
+++ src/Lab1/Student.h
@@ -5,12 +5,11 @@
 #include <tchar.h>
 #include "TypeStream.h"
 
-template <typename TYPESTRING>
 class Student
 {
 public:
 	Student();
-	Student(const Student<TYPESTRING>&);
+	Student(const Student&);
 
 	void setName(const std::basic_string<TYPESTRING>&);
 	std::basic_string<TYPESTRING> getName() const;
@@ -27,21 +26,19 @@ public:
 	void setAverageGrade(float);
 	float getAverageGrade() const;
 
-	const Student<TYPESTRING>& operator=(const Student<TYPESTRING>&);
-	bool operator==(const Student<TYPESTRING>&) const;
-	bool operator!=(const Student<TYPESTRING>&) const;
-	bool operator>=(const Student<TYPESTRING>&) const;
-	bool operator<=(const Student<TYPESTRING>&) const;
-	bool operator>(const Student<TYPESTRING>&) const;
-	bool operator<(const Student<TYPESTRING>&) const;
+	const Student& operator=(const Student&);
+	bool operator==(const Student&) const;
+	bool operator!=(const Student&) const;
+	bool operator>=(const Student&) const;
+	bool operator<=(const Student&) const;
+	bool operator>(const Student&) const;
+	bool operator<(const Student&) const;
 
-	template <typename TYPESTRING>
-	friend listStream& operator<<(listStream&, const Student<TYPESTRING>&);
-	template <typename TYPESTRING>
-	friend listInStream& operator>>(listInStream&, Student<TYPESTRING>&);
+	friend listStream& operator<<(listStream&, const Student&);
+	friend listInStream& operator>>(listInStream&, Student&);
 
 private:
-	int compareStudents(const Student<TYPESTRING>&) const;
+	int compareStudents(const Student&) const;
 
 private:
 	std::basic_string<TYPESTRING> m_Name {};
@@ -50,171 +47,3 @@ private:
 	short int m_BirthYear;
 	float m_AverageGrade;
 };
-
-template <typename TYPESTRING>
-Student<TYPESTRING>::Student()
-	: m_BirthYear(0),
-	m_AverageGrade(0.0)
-{
-
-}
-
-template <typename TYPESTRING>
-Student<TYPESTRING>::Student(const Student<TYPESTRING>& someStudent)
-	: m_Name(someStudent.m_Name),
-	m_Surname(someStudent.m_Surname),
-	m_Lastname(someStudent.m_Lastname),
-	m_BirthYear(someStudent.m_BirthYear),
-	m_AverageGrade(someStudent.m_AverageGrade)
-{
-
-}
-
-template <typename TYPESTRING>
-void Student<TYPESTRING>::setName(const std::basic_string<TYPESTRING>& name)
-{
-	m_Name = name;
-}
-
-template <typename TYPESTRING>
-std::basic_string<TYPESTRING> Student<TYPESTRING>::getName() const
-{
-	return m_Name;
-}
-
-template <typename TYPESTRING>
-void Student<TYPESTRING>::setSurname(const std::basic_string<TYPESTRING>& surname)
-{
-	m_Surname = surname;
-}
-
-template <typename TYPESTRING>
-std::basic_string<TYPESTRING> Student<TYPESTRING>::getSurname() const
-{
-	return m_Surname;
-}
-
-template <typename TYPESTRING>
-void Student<TYPESTRING>::setLastname(const std::basic_string<TYPESTRING>& lastname)
-{
-	m_Lastname = lastname;
-}
-
-template <typename TYPESTRING>
-std::basic_string<TYPESTRING> Student<TYPESTRING>::getLastname() const
-{
-	return m_Lastname;
-}
-
-template <typename TYPESTRING>
-void Student<TYPESTRING>::setBirthYear(int year)
-{
-	m_BirthYear = year;
-}
-
-template <typename TYPESTRING>
-short int Student<TYPESTRING>::getBirthYear() const
-{
-	return m_BirthYear;
-}
-
-template <typename TYPESTRING>
-void Student<TYPESTRING>::setAverageGrade(float averageGrade)
-{
-	m_AverageGrade = averageGrade;
-}
-
-template <typename TYPESTRING>
-float Student<TYPESTRING>::getAverageGrade() const
-{
-	return m_AverageGrade;
-}
-
-template <typename TYPESTRING>
-const Student<TYPESTRING>& Student<TYPESTRING>::operator=(const Student& right)
-{
-	if (this != &right)
-	{
-		m_Name = right.m_Name;
-		m_Surname = right.m_Surname;
-		m_Lastname = right.m_Lastname;
-		m_BirthYear = right.m_BirthYear;
-		m_AverageGrade = right.m_AverageGrade;
-	}
-
-	return *this;
-}
-
-template <typename TYPESTRING>
-int Student<TYPESTRING>::compareStudents(const Student<TYPESTRING>& right) const
-{
-	int result = m_Surname.compare(right.m_Surname);
-
-	if (result == 0)
-	{
-		result = m_Name.compare(right.m_Name);
-
-		if (result == 0)
-		{
-			return m_Lastname.compare(right.m_Lastname);
-		}
-	}
-
-	return result;
-}
-
-template <typename TYPESTRING>
-bool Student<TYPESTRING>::operator==(const Student<TYPESTRING>& right) const
-{
-	return compareStudents(right) == 0;
-}
-
-template <typename TYPESTRING>
-bool Student<TYPESTRING>::operator!=(const Student<TYPESTRING>& right) const
-{
-	return compareStudents(right) != 0;
-}
-
-template <typename TYPESTRING>
-bool Student<TYPESTRING>::operator>=(const Student<TYPESTRING>& right) const
-{
-	return compareStudents(right) != -1;
-}
-
-template <typename TYPESTRING>
-bool Student<TYPESTRING>::operator<=(const Student<TYPESTRING>& right) const
-{
-	return compareStudents(right) != 1;
-}
-
-template <typename TYPESTRING>
-bool Student<TYPESTRING>::operator>(const Student<TYPESTRING>& right) const
-{
-	return compareStudents(right) == 1;
-}
-
-template <typename TYPESTRING>
-bool Student<TYPESTRING>::operator<(const Student<TYPESTRING>& right) const
-{
-	return compareStudents(right) == -1;
-}
-
-template <typename TYPESTRING>
-listStream& operator<<(listStream& output, const Student<TYPESTRING>& right)
-{
-	output << right.m_Surname << __T(" ") << right.m_Name << __T(" ")
-		<< right.m_Lastname << __T(" ") << right.m_BirthYear << __T(" ")
-		<< right.m_AverageGrade << std::endl;
-
-	return output;
-}
-
-template <typename TYPESTRING>
-listInStream& operator >> (listInStream& input, Student<TYPESTRING>& right)
-{
-	input >> right.m_Surname >> right.m_Name >> right.m_Lastname
-		>> right.m_BirthYear >> right.m_AverageGrade;
-
-	return input;
-}
-
