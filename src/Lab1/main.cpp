9f6f87a260d600b2f40ace28793cb9b0c59741a2
ableigdev <igor.yarovoy.98@ukr.net>
2018-04-25 10:29:45 +0300

    Added code in main function

--- src/Lab1/main.cpp
+++ src/Lab1/main.cpp
@@ -7,64 +7,87 @@
 #include "NameList.h"
 #include "blist.h"
 
-#ifdef UNICODE
-#define out std::wcout
-#define typeString wchar_t
-#else
-#define out std::cout
-#define typeString char
-#endif // UNICODE
-
 
 int main()
 {
 	std::srand(time(0));
-	
 	{
-		TBList<int> list;
+		List<int> list;
 
-		for (int i = 0; i < 1000000; ++i)
+		for (int i = 0; i < 5; ++i)
 		{
-			list.AddBeforeHead(rand() % 3000000);
+			list.pushBack(i);
+		}
+
+		for (int i = 0; i < 5; ++i)
+		{
+			list.pushBack(rand() % 3000000);
 		}
 
 		time_t start = clock();
-		list.SortListBiDir();
+		list.sort();
 		time_t finish = clock();
 
-		out << "time: " << finish - start << std::endl;
-		out << "Counter compare: " << list.getCounterCompare() << std::endl;
-		out << "Counter recursive call: " << list.getCounterRecursiveCall() << std::endl;
-	}
-	{
-		
-		List<int> lists;
-
-		for (int i = 0; i < 1000000; ++i)
+		if (list.bubbleSort())
 		{
-			lists.pushFront(rand() % 3000000);
-		}
-
-		time_t start = clock();
-		lists.sort();
-		time_t finish = clock();
-
-		out << "time: " << finish - start << std::endl;
-
-		if (lists.bubbleSort())
-		{
-			out << "Sort good" << std::endl;
+			std::cout << "good" << std::endl;
 		}
 		else
 		{
-			out << "Sort bad" << std::endl;
+			std::cout << "bad" << std::endl;
 		}
 
-		out << "Counter compare: " << lists.getCounterCompare() << std::endl;
-		out << "Counter recursive call: " << lists.getCounterRecursiveCall() << std::endl;
+		out << list << std::endl;
+
+		std::cout << "time: " << finish - start << std::endl;
+		out << "Counter compare: " << list.getCounterCompare() << std::endl;
+		out << "Counter recursive call: " << list.getCounterRecursiveCall() << std::endl;
+	}
+	
+	{
+	TBList<int> list;
+
+	for (int i = 0; i < 1000000; ++i)
+	{
+	list.AddBeforeHead(rand() % 3000000);
 	}
 
-	std::cin.get();
+	time_t start = clock();
+	list.SortListBiDir();
+	time_t finish = clock();
+
+	out << "time: " << finish - start << std::endl;
+	out << "Counter compare: " << list.getCounterCompare() << std::endl;
+	out << "Counter recursive call: " << list.getCounterRecursiveCall() << std::endl;
+	}
+	{
+
+	List<int> lists;
+
+	for (int i = 0; i < 1000000; ++i)
+	{
+	lists.pushFront(rand() % 3000000);
+	}
+
+	time_t start = clock();
+	lists.sort();
+	time_t finish = clock();
+
+	out << "time: " << finish - start << std::endl;
+
+	if (lists.bubbleSort())
+	{
+	out << "Sort good" << std::endl;
+	}
+	else
+	{
+	out << "Sort bad" << std::endl;
+	}
+
+	out << "Counter compare: " << lists.getCounterCompare() << std::endl;
+	out << "Counter recursive call: " << lists.getCounterRecursiveCall() << std::endl;
+	}
+	
 	system("pause");
 
 	return 0;
